c5dba1d59bff60e3270420546fb6e240c6ca08c7
Shawrov <intshawrov@gmail.com>
2025-12-26 17:11:19 +0600

    Updated

---
+++ header.php
@@ -0,0 +1,97 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+    <meta charset="<?php bloginfo('charset'); ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?php bloginfo('name'); ?>-- <?php bloginfo('description'); ?></title>
+
+
+    <!----- jQuery google cdn ----->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
+
+    
+
+    <!-- Boxicons CDN -->
+    <link href="https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css" rel="stylesheet">
+
+
+    <?php wp_head(); ?>
+</head>
+<body <?php body_class(); ?>>
+    <header>
+        <div class="container">
+            <div class="header-wrapper">
+                <!----- logo ----->
+                <div class="logo">
+                    <a href="index.html">
+                        <img src="<?php echo get_template_directory_uri(); ?>/assets/images/logo.png" alt="logo">
+                    </a>
+                </div>
+
+                <!----- main-menu and button ----->
+                <div class="menu-button-wrapper">
+                    <nav class="main-menu">
+                        <ul>
+                            <li><a class="active" href="#">Home</a></li>
+                            <li><a href="#">Courses</a></li>
+                            <li><a href="#">Pricing</a></li>
+                            <li><a href="#">Reviews</a></li>
+                        </ul>
+
+                    </nav>
+
+                    <!----- sign-up-btn ----->
+                    <div class="button btn-black">
+                        <a href="#">Sign Up</a>
+                    </div>
+
+
+                    <!----- phone menu ----->
+                    <div class="menu-btn">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
+                            <path d="M3 6h18M3 12h18M3 18h18" stroke="currentColor" stroke-width="2" stroke-linecap="round" />
+                        </svg>
+                    </div>
+                </div>
+
+                <!----- menu item for phone ----->
+                <div  id="navPhone" class="menu-item-phone">
+
+                    <!----- logo ----->
+                    <div class="logo-div">
+                        <div class="logo">
+                            <a href="index.html">
+                                <img src="<?php echo get_template_directory_uri(); ?>/assets/images/logo.png" alt="logo">
+                            </a>
+                        </div>
+
+                        <div class="x-icon menu-btn">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" stroke="currentColor" stroke-width="2" viewBox="0 0 24 24">
+                                <line x1="18" y1="6" x2="6" y2="18" stroke-linecap="round" />
+                                <line x1="6" y1="6" x2="18" y2="18" stroke-linecap="round" />
+                            </svg>
+                        </div>
+                    </div>
+                    
+
+
+                    <div class="menu-div">
+                        <nav class="main-menu-phone">
+                            <ul>
+                                <li><a class="active" href="#">Home</a></li>
+                                <li><a href="#">Courses</a></li>
+                                <li><a href="#">Pricing</a></li>
+                                <li><a href="#">Reviews</a></li>
+                            </ul>
+
+                        </nav>
+
+                        <!----- sign-up-btn ----->
+                        <div class="button btn-black">
+                            <a href="#">Sign Up</a>
+                        </div>
+                    </div>    
+                </div>
+            </div>
+        </div>
+    </header>
