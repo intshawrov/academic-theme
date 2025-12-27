5ee33198d0af583a1b155be8697dbc67377b6da4
Shawrov <intshawrov@gmail.com>
2025-12-27 18:31:08 +0600

    Update page.php

--- page.php
+++ page.php
@@ -1,25 +1,22 @@
-<?php get_header(); ?>
+<?php 
+get_header(); 
+?>
+
+
+<section class="container">
+<?php
+      if(have_posts() ):
+      while( have_posts() ): the_post(); 
+
+              the_title();
+
+endwhile;
+endif;
+?>
+</section>
 
 
 
-
-
-<?php if(have_posts()):
-while(have_posts() ): the_post(); ?>
-
-<?php endwhile;
-endif; ?>
-
-
-
-
-
-
-
-
-
-
-
-
-
- <?php get_footer(); ?>
+<?php 
+get_footer(); 
+?>
