9035621637629f2a7083ad19a8904f91f2e61d05
Shawrov <intshawrov@gmail.com>
2025-12-31 18:52:10 +0600

    Update functions.php

--- functions.php
+++ functions.php
@@ -321,19 +321,5 @@ function lessonlms_customaiz_register($wp_customize){
 add_action('customize_register', 'lessonlms_customaiz_register');
 
 
-function lessonlms_theme_widgets(){
-    register_sidebar( array(
-        'name'          => __( 'Sidebar Widget Area', 'lessonlms' ),
-        'id'            => 'sidebar-1',
-        'description'   => __( 'Add widgets here to appear in your sidebar.', 'lessonlms' ),
-        'before_widget' => '<section id="%1$s" class="widget %2$s">',
-        'after_widget'  => '</section>',
-        'before_title'  => '<h2 class="widget-title">',
-        'after_title'   => '</h2>',
-    ) );
 
-
-}
-
-add_action('widgets_init', 'lessonlms_theme_widgets');
 
